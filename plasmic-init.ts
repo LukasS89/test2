58f773cf84601db3742b82db67c74abfd8b89866
LukasS89 <lukas.sakala@gmail.com>
2024-11-06 12:31:45 +0100

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,7 +1,5 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-import Login from './components/Login';
-import Logout from './components/Logout';
-import { AuthGlobalContext } from './components/AuthGlobalContext';
+
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -13,38 +11,3 @@ export const PLASMIC = initPlasmicLoader({
   preview: false,
 });
 
-// Register the Login component
-PLASMIC.registerComponent(Login, {
-  name: 'Login',
-  importPath: './components/Login', // Path to the component
-  props: {
-    email: { type: 'string' }, // Define expected props
-    password: { type: 'string' },
-  },
-  refActions: {
-    triggerLogin: {
-      description: 'Triggers the login action with provided credentials.',
-      argTypes: [] // No arguments needed for this action
-    }
-  },
-});
-
-// Register the Logout component
-PLASMIC.registerComponent(Logout, {
-  name: 'Logout',
-  importPath: './components/Logout', // Path to the component
-  props: {},
-  refActions: {
-    triggerLogout: {
-      description: 'Triggers the logout action.',
-      argTypes: [],
-    },
-  },
-});
-
-// Register global context
-PLASMIC.registerGlobalContext(AuthGlobalContext, {
-  name: "AuthGlobalContext",
-  props: { authUrl: "string" },
-  providesData: true,
-});
